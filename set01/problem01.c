c8c456093b62fd5c13dfe0162c9c2804e90cd38b
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-11 13:08:30 +0530

    committed 10/11/2023, 1:08:29 PM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,10 +1,11 @@
 // 1. Write a C program to print your name.
 #include<stdio.h>
 
-int main(){
-    for (int i = 0; i < 3; i++)
-    {
-        printf("Hello World")
-    }
+int main()
+{
     
+    {
+        printf("Pranavi\n");
+    }
+    return 0;
 }
