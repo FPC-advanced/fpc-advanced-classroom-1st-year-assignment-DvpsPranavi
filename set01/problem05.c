6ce0cb70eee4166473242be2cb17c2dd20a96ffa
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-16 10:11:32 +0530

    committed 10/16/2023, 10:11:32 AM

--- set01/problem05.c
+++ set01/problem05.c
@@ -10,5 +10,44 @@ int main()
     a=input();
     b=input();
     c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
 }
 
+int input()
+{
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    return n;
+}
+
+int compare(int a, int b, int c)
+{
+    int i,n;
+    for(i=0;i<n;i++)
+    {
+        if(a>=b &&a>=c)
+        {
+            return a;
+        } 
+
+        if(b>=a &&b>=c);
+        {
+            return b;
+        }  
+
+        else
+        {
+            return c;
+        }
+    }
+
+    return 0;
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest number between %d,%d and %d is %d",a,b,c,largest);
+}
