700f543e62e9a318134bb2d81cc7fe9b01ad94fd
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-16 09:59:23 +0530

    committed 10/16/2023, 9:59:23 AM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,14 @@
+//5. Write a C program to compare three numbers using **pass by value**
+#include <stdio.h>
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+}
 
