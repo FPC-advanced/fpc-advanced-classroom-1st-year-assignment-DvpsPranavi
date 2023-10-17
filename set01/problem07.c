2e7333f4a80def4db93be12a5a0e74cc7555cfb4
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-17 07:02:43 +0530

    committed 10/17/2023, 7:02:42 AM

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,2 +1,39 @@
+#include <stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+
+int main()
+{
+    int n,sum=0;
+    n=input();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
+
+int input()
+{
+    int x;
+    printf("Enter the number of natural numbers to be added");
+    scanf("%d",&x);
+    return x;
+}
+
+int sum_n_nos(int n)
+{
+    int sum;
+    for(int i =1; i<= n;i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+
+}
+
+void output(int n, int sum)
+{
+    printf("The sum of the first %d natural numbers is %d \n",n,sum);
+}
 
 
