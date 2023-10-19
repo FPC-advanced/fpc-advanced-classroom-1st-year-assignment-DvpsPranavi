79099871b46463027ca1b164cbe8126d0099d681
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-19 15:52:37 +0530

    committed 10/19/2023, 3:52:37 PM

---
+++ set01/problem09.c
@@ -0,0 +1,10 @@
+//9. Write a C program to find the [square root] of a number.
+#include <stdio.h>
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
+
+int main()
+{
+    
+}
