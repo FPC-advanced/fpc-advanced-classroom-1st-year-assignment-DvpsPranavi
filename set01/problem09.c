e136d41b4b79e8837444680d50c1764082598395
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-19 16:29:21 +0530

    committed 10/19/2023, 4:29:20 PM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,10 +1,49 @@
 //9. Write a C program to find the [square root] of a number.
 #include <stdio.h>
+
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
 
-int main()
-{
-    
+int main() {
+    float number, sqrRoot;
+
+    number = input(); 
+    sqrRoot = square_root(number); 
+    output(number, sqrRoot); 
+
+    return 0;
+}
+
+
+float input() {
+    float num;
+    printf("Enter a number: ");
+    scanf("%f", &num);
+    return num;
+}
+
+
+float square_root(float n) {
+    if (n < 0) {
+        return -1.0f; 
+    }
+
+    float estimate = 1.0f;
+    float prevEstimate = 0.0f;
+
+    while (estimate != prevEstimate) {
+        prevEstimate = estimate;
+        estimate = 0.5f * (estimate + n / estimate);
+    }
+
+    return estimate;
+}
+
+void output(float n, float sqrroot) {
+    if (sqrroot < 0) {
+        printf("Cannot calculate the square root of a negative number.\n");
+    } else {
+        printf("The square root of %.2f is approximately %.6f\n", n, sqrroot);
+    }
 }
