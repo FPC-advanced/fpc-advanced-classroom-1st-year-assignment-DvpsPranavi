49013b4e154ff9caae2f4e8b58088458e5b1181a
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-30 09:49:24 +0530

    committed 10/30/2023, 9:49:23 AM

--- set01/problem11.c
+++ set01/problem11.c
@@ -6,6 +6,6 @@ struct _complex {
 typedef struct _complex Complex;
 
 Complex input_complex();
-void print_complex(Complex c);
+
 Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
