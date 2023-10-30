a0c90c43b4a93bb0fea7b239f1a554014eca2a75
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-10-30 09:29:19 +0530

    committed 10/30/2023, 9:29:18 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -3,4 +3,7 @@ void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 
-int mian
+int main()
+{
+    
+}
