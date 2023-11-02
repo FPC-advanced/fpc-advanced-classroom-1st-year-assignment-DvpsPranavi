f6306c0eb39bb0cb4ba1a5de20d90c3f39c542dd
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-11-02 11:01:08 +0530

    committed 11/2/2023, 11:01:07 AM

--- set02/problem01.c
+++ set02/problem01.c
@@ -27,5 +27,5 @@ void find_area(float base , float height, float *area)
 
 void output(float base, float height, float area)
 {
-    printf("The area of the triangle is ")
+    printf("The area of the triangle with base%f ,height%f is %f")
 }
