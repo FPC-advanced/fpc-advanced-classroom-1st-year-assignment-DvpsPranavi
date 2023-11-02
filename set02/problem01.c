992880afb49effcd452965714aedac0f92a840a2
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2023-11-02 11:09:08 +0530

    committed 11/2/2023, 11:09:07 AM

--- set02/problem01.c
+++ set02/problem01.c
@@ -14,9 +14,9 @@ int main()
 void input(float *base,float *height)
 {
     printf("Enter the base of the triangle\n");
-    scanf("%f",&base);
+    scanf("%f",base);
     printf("Enter the height of the triangle\n");
-    scanf("%f",&height);
+    scanf("%f",height);
 }
 
 void find_area(float base , float height, float *area)
